80023a8053f36557bfaabb904d4890b5c74f17e3
Marco de Freitas <marcogfreitas@gmail.com>
2023-08-16 18:18:39 +0100

    Update README.md

--- src/Data/IdDelegatingHandler.cs
+++ src/Data/IdDelegatingHandler.cs
@@ -2,7 +2,7 @@
 {
     // It might be overkill to create a delegating handler for this, but it's a good example of how to do it.
     // It's also a good example of how to use the IHttpContextAccessor to get the customer id from the route.
-    // This approach allow the repository to focus solely on the product and not on the customer details.
+    // This approach allow the repositories to focus solely on their purpose and not on other details.
 
     public class IdDelegatingHandler : DelegatingHandler
     {
@@ -17,7 +17,6 @@
                                                                CancellationToken cancellationToken)
         {
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
-#pragma warning disable CS8604 // Possible null reference argument.
             Guid customerId = Guid.Parse(_httpContextAccessor.HttpContext.Request.RouteValues["id"]
                                          as string ?? Guid.Empty.ToString());
             // Skip at all if there is no customer id
@@ -27,7 +26,6 @@
             }
             Guid productId = Guid.Parse(_httpContextAccessor.HttpContext.Request.RouteValues["productId"] as string
                                         ?? Guid.Empty.ToString());
-#pragma warning restore CS8604 // Possible null reference argument.
 #pragma warning restore CS8602 // Dereference of a possibly null reference.
 #pragma warning disable CS8602 // Dereference of a possibly null reference.
             request.RequestUri = new Uri(uriString: request.RequestUri.OriginalString.Replace(oldValue: "{id}",
